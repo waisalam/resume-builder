8b13acc02f67aac40d11ca7b1317121b780f3208
waisalam <waisalam9523@gmail.com>
2026-05-27 17:12:04 +0530

    fixed prisma ts file issue

--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as {
